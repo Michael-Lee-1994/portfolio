1adce9c38cc6e0f274515b77f92003143e72f5a0
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2021-04-04 17:16:02 -0700

    added about bio infomation

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,20 @@ class About extends Component {
                 <h1>
                     About
                 </h1>
-                
+
                 <h2>
                     A little about me
                 </h2>
 
                 <div>
                     <div>
-                        <h4>date</h4>
-                        <h4>title</h4>
+                        <h4>December 1994</h4>
+                        <h4>My Story</h4>
                     </div>
                     <div>
                         <p>
-                            summary
+                        Born in the humble city of Federal Way, WA, and raised in multiple different
+                        areas in TX and Korea, I was constantly tested and pushed to explore who I was as an individual. Through this diversity, I found many different passions in sports(Volleyball, Basketball, Football), arts(Photography & Digital Media), games(Video & Board), and music(Most Genre), all of which shaped the person I am today.
                         </p>
                     </div>
                 </div>
@@ -50,36 +51,36 @@ class About extends Component {
 
                 <div>
                      <div>
-                        <h4>date</h4>
-                        <h4>title</h4>
+                        <h4>2018-2019</h4>
+                        <h4>University of Washington Full Stack Development</h4>
                     </div>
                     <div>
                         <p>
-                            summary
+                        I took part in an intensive 6-month long coding boot camp dedicated to designing and building web applications, in which I graduated with top marks. I learned skills consisting of HTML, CSS, Javascript, JQuery, Bootstrap, Firebase, MERN(MongoDB, Express, React.js, Node.js), MySQL, Handelbars.js.
                         </p>
                     </div>
                 </div>
 
                 <div>
                      <div>
-                        <h4>date</h4>
-                        <h4>title</h4>
+                        <h4>2013-2017</h4>
+                        <h4>SPU Bachelor of Arts, Political Science</h4>
                     </div>
                     <div>
                         <p>
-                            summary
+                        I spent 4 years at Seattle Pacific University, in which I graduated with a degree in Political Science. It was here where I developed various skills and techniques.
                         </p>
                     </div>
                 </div>
 
                 <div>
                      <div>
-                        <h4>date</h4>
-                        <h4>title</h4>
+                        <h4>August 2014-Current</h4>
+                        <h4>Work Experience</h4>
                     </div>
                     <div>
                         <p>
-                            summary
+                        I started my first job over the summer of 2014 as a footwear salesman at Sports Authority. And since then I've worked many different jobs in various different fields, such as a Urology Lab assistant, a Volleyball Supervisor, a hospital volunteer, and youths sports coach.
                         </p>
                     </div>
                 </div>
